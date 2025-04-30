ba50e57aaebcae447b037de7c903410f392e68c2
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2025-04-30 12:24:34 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'calandar',
+  webDir: 'www'
+};
+
+export default config;
